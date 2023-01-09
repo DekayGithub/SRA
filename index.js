d659eb30b636474cda8a3dcc55c7b9dcc0ce0d59
Devin Kapla <devinkapla@gmail.com>
2023-01-09 16:04:01 -0600

    get all data with basic logic

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-const rp = require('request-promise');
+const rp = require("request-promise");
 
-const TEAMS_API_URL = 'https://statsapi.web.nhl.com/api/v1/teams'
+const TEAMS_API_URL = "https://statsapi.web.nhl.com/api/v1/teams";
+const TEAMS_SCHEDULE_URL = "https://statsapi.web.nhl.com/api/v1/schedule";
 
-const getAllTeams = () => 
+const getData = (uri, qs) =>
   rp({
-    uri: TEAMS_API_URL,
-    method: 'GET',
-    json: true
-    })
+    uri,
+    method: "GET",
+    ...(qs ? { qs } : {}),
+    json: true,
+  });
 
-const startNHLPipeline = async () => {
+const startNHLPipeline = async (teamId, season) => {
   try {
     // extract
-    // transform
-    // load
-  } catch (error){
-    console.log('NHL Pipeline error:', error)
-  }
-}
+    const teamData = await getData(TEAMS_API_URL + `/${teamId}`);
+    const { id, name, venue } = teamData.teams[0];
 
-getAllTeams().then(data => console.log(data));
+    const teamStatData = await getData(TEAMS_API_URL + `/${teamId}/stats`, {
+      season,
+    });
+    const {
+      gamesPlayed,
+      wins,
+      losses,
+      pts: points,
+      goalsPerGame,
+    } = teamStatData.stats[0].splits[0].stat;
+
+    const teamSchedule = await getData(TEAMS_SCHEDULE_URL, {
+      season,
+      teamId,
+    });
+    const teamsPlayed = Object.values(teamSchedule.dates[0].games[0].teams).map(
+      (team) => ({ id: team.team.id, name: team.team.name })
+    );
+    const firstGameOpponent = teamsPlayed.filter((team) => team.id !== teamId);
+    const firstGameDate = teamSchedule.dates[0].date;
+    // transform
+
+    // load
+  } catch (error) {
+    console.log("NHL Pipeline error:", error);
+  }
+};
+
+startNHLPipeline(5, "20032004");
